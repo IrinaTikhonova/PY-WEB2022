f0308d8861b2f5fa25a7817f003234cb5992d187
Тихонова Ирина Геннадьевна <sondrafinchley@mail.ru>
2023-02-19 16:58:37 +0200

    non authorized added

--- apps/home/views.py
+++ apps/home/views.py
@@ -10,15 +10,19 @@ class IndexShopView(View):
         fill_id_card_in_session(request)
 
         data = Product.objects.all()
-        wishlist = WishlistItem.objects.filter(wishlist__user=request.user)
-        products = [item.product.name for item in wishlist]
-        cart = CartItemShop.objects.filter(cart__user=request.user)
-        products_cart = [item.product.name for item in cart]
-        context = {'data': data,
-                   'wishlist_items': products,
-                   'cart_items': products_cart
-                   }
-        return render(request, 'home/index.html', context)
+        if request.user.is_authenticated:
+            wishlist = WishlistItem.objects.filter(wishlist__user=request.user)
+            products = [item.product.name for item in wishlist]
+            cart = CartItemShop.objects.filter(cart__user=request.user)
+            products_cart = [item.product.name for item in cart]
+            context = {'data': data,
+                    'wishlist_items': products,
+                    'cart_items': products_cart
+                    }
+            return render(request, 'home/index.html', context)
+        else:
+            context = {'data': data}
+            return render(request, 'home/index.html', context)
 
 class AboutView(View):
     def get(self, request):
